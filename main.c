4c087831a28baabdd9fcc66f2c4e195487443438
dedeksaleha <dedek24@mhs.usk.ac.id>
2025-05-14 08:52:10 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,24 +1,15 @@
 #include <stdio.h>
 #include "fungsi.h"
 
-int main() {
-    initQueue();
-    muatBukuDariFile();
-
-    int opsi;
-    do {
-        printf("\n=== MENU UTAMA ===\n");
-        printf("1. Register Admin\n");
-        printf("2. Register User\n");
-        printf("3. Login Admin\n");
-        printf("4. Login User\n");
-        printf("5. Keluar\n");
+int inputAngka() {
+    int angka;
+    while (1) {
         printf("Pilih: ");
-        scanf("%d", &opsi);
-
-        switch (opsi) {
-            case 1: registerAdmin(); break;
-            case 2: registerUser(); break;
-            case 3:
-                if (loginAdmin()) menuAdmin();
-                else printf("Login admin gagal. Coba lagi.\n");
+        if (scanf("%d", &angka) == 1)
+            return angka;
+        else {
+            printf("Input tidak valid. Harap masukkan angka.\n");
+            while (getchar() != '\n'); // flush stdin
+        }
+    }
+}
