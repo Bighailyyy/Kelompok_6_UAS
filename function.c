e8beb4fc977500f77ec1cf97983792cae3c3d8d1
dedeksaleha <dedek24@mhs.usk.ac.id>
2025-05-14 08:58:16 +0700

    Update function.c

--- function.c
+++ function.c
@@ -60,3 +60,64 @@ int idBukuSudahAda(int id) {
     return 0;
 }
 
+void tambahBuku() {
+    Buku b;
+    printf("ID Buku: "); scanf("%d", &b.id);
+    if (idBukuSudahAda(b.id)) {
+        printf("ID buku sudah digunakan!\n");
+        return;
+    }
+    getchar();
+    printf("Judul: "); fgets(b.judul, 100, stdin);
+    printf("Penulis: "); fgets(b.penulis, 100, stdin);
+    printf("Genre: "); fgets(b.genre, 50, stdin);
+    printf("Stok: "); scanf("%d", &b.stok);
+
+    b.judul[strcspn(b.judul, "\n")] = 0;
+    b.penulis[strcspn(b.penulis, "\n")] = 0;
+    b.genre[strcspn(b.genre, "\n")] = 0;
+
+    daftarBuku[jumlahBuku++] = b;
+    simpanBukuKeFile();
+    printf("Buku berhasil ditambahkan.\n");
+}
+
+void tampilkanBuku() {
+    if (jumlahBuku == 0) {
+        printf("Belum ada buku yang tersedia.\n");
+        return;
+    }
+
+    for (int i = 0; i < jumlahBuku; i++) {
+        printf("[%d] %s oleh %s | Genre: %s | Stok: %d\n",
+               daftarBuku[i].id, daftarBuku[i].judul,
+               daftarBuku[i].penulis, daftarBuku[i].genre, daftarBuku[i].stok);
+    }
+}
+
+void editBuku() {
+    int id, found = 0;
+    printf("Masukkan ID buku yang ingin diedit: ");
+    scanf("%d", &id);
+    for (int i = 0; i < jumlahBuku; i++) {
+        if (daftarBuku[i].id == id) {
+            getchar();
+            printf("Judul Baru: "); fgets(daftarBuku[i].judul, 100, stdin);
+            printf("Penulis Baru: "); fgets(daftarBuku[i].penulis, 100, stdin);
+            printf("Genre Baru: "); fgets(daftarBuku[i].genre, 50, stdin);
+            printf("Stok Baru: "); scanf("%d", &daftarBuku[i].stok);
+
+            daftarBuku[i].judul[strcspn(daftarBuku[i].judul, "\n")] = 0;
+            daftarBuku[i].penulis[strcspn(daftarBuku[i].penulis, "\n")] = 0;
+            daftarBuku[i].genre[strcspn(daftarBuku[i].genre, "\n")] = 0;
+
+            found = 1;
+            simpanBukuKeFile();
+            printf("Buku berhasil diedit.\n");
+            break;
+        }
+    }
+    if (!found) printf("Buku tidak ditemukan.\n");
+}
+
+
